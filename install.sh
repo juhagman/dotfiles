59e52a5d7c344b049636ad7ecf278d8439853f1b
Jussi Hagman <jussi.hagman@hublet.fi>
2020-12-23 01:16:16 +0200

    Add zshrc

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 # ln -s ~/dotfiles/vim/ftplugin ~/.vim
 # ln -s ~/dotfiles/vim/coc-settings.json ~/.vim/
 
+# Zsh
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+
 # Kitty
 # mkdir -p ~/.config/kitty
 # ln -s ~/dotfiles/kitty/kitty.conf ~/.config/kitty/
